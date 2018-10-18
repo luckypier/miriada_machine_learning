75d4f2f18d4c15475541af1d958e6224d91d2452
Pierre Recuay <alonepier@hotmail.com>
2018-10-18 00:08:38 -0500

    update module 02

--- 01_introducción/01_introducción_anotaciones.js
+++ 01_introducción/01_introducción_anotaciones.js
@@ -55,3 +55,48 @@
 			- Feature engineering para el aprendizaje
 
 	-los algoritmos  de aprendizaje
+
+/*
+ * MÓDULO 3. Soluciones De Machine Learning: Aprendizaje Supervisado
+ */
+
+	Video A
+		- Problemas de aprendizaje supervisado: el modelo aprende de datos etiquetados por un experto.
+			- clasificación, modelo árbol de decisión
+			- regresión
+		- Aprendizaje no supervisado: el modelo aprende de datos no etiquetados.
+			- como el clustering, detección de anormalias y las asociaciones.
+ 
+	- Clasificación y regresión: árboles de decisión 
+	
+//xxxxxxxxxxxxxx
+1 ¿Se pueden usar estos datos para determinar si un nuevo paciente es diabético usando Machine Learning?
+Sí, porque las tablas incluyen información relevante sobre los factores que pueden influir en el diagnóstico para cada paciente y el propio diagnóstico.
+2. Respecto al formato de los datos de la pregunta anterior, ¿están en un formato que permita usarlos en Machine Learning?
+No todos, ya que sólo los datos de la tabla de diagnósticos tienen la estructura necesaria: una fila por usuario y una columna por cada propiedad o atributo que queremos usar en el aprendizaje.
+3. ¿Qué transformaciones o procesos deberíamos utilizar para preparar dichos datos para el aprendizaje?
+Deberíamos usar pivoting sobre las filas de cada usuario de la tabla de pruebas analíticas y construir una única tabla con una fila por paciente. En dicha tabla tendríamos por columnas: glucosa, insulina, edad y diagnóstico de cada paciente.
+4. Los tipos de cada campo que usamos en Machine Learning determinan cómo va a tratar los datos el algoritmo para aprender, y por lo tanto es importante que sean correctos.¿Cuáles son los tipos de cada uno de los campos que contiene el fichero Diabetes_es_ext.csv?
+El campo Fecha de diagnóstico es de tipo fecha/hora y se ha expandido automáticamente en sus componentes. Todos los demás son numéricos excepto el campo Diabetes que es categórico, el campo Medicación previa que es de tipo items y el campo Observaciones que es de tipo texto.
+5. ¿Cuáles de estas afirmaciones son ciertas?
+El Dataset contiene 768 instancias útiles para el Machine Learning, aunque los campos Medicación previa y Observaciones tienen valores ausentes (754 y 755 respectivamente). El campo Fecha de diagnóstico contiene dos errores y los campos derivados de él (Fecha de diagnóstico.year, etc.) estarán ausentes en esos dos casos. El rango de valores del campo Glucosa va del 0 al 199.
+
+Simple present
+	I use the Simple Present for repeated actions.
+
+	He doesn't speak French.
+	Does Nina play computer games?
+	Does Andy do the shopping?
+
+
+
+lay the table
+good marks
+look at
+she tidies up her room
+water the flowers
+
+
+javierquinones.
+Foto del perfil de javierquinones.
+javierquinones.languagecoach@gmai.com
